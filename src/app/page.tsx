bc249fb09d3c6f759da73745c4bb066636dd868f
Abdel-manan Abdel-rahman <abdelmanan.abdelrahman03@gmail.com>
2023-12-15 11:20:32 +0300

    type errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,23 @@
 "use client"
-import Editor from "@/components/atoms/editor"
-import EditorContainer from "@/components/layout/editor-container"
-//import Settings from "@/components/layout/settings"
+//import Editor from "@/components/atoms/editor"
+//import EditorContainer from "@/components/layout/editor-container"
+import Settings from "@/components/layout/settings"
 import React from "react"
 
 export default function Home() {
-  const [openSettings, setOpenSettings] = React.useState(false)
+  const [openSettings, setOpenSettings] = React.useState(true)
   const toggleSettings = () => setOpenSettings(!openSettings);
   return (
     <main className="flex flex-col items-center bg-gray-100">
-    <EditorContainer logs={true} toggleSettings={toggleSettings} >
-      <Editor/>
-    </EditorContainer>
-    {/**
     {
       openSettings && (
       <Settings toggleSettings={toggleSettings}/>
       )
       }
+    {/**
+    <EditorContainer logs={true} toggleSettings={toggleSettings} >
+      <Editor/>
+    </EditorContainer>
     */}
     </main>
   )
