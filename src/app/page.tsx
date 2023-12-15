d129ccba349138968ac682081a820908f408613b
Abdel-manan Abdel-rahman <abdelmanan.abdelrahman03@gmail.com>
2023-12-15 11:30:14 +0300

    type errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,18 @@ import Settings from "@/components/layout/settings"
 import React from "react"
 
 export default function Home() {
-  const [openSettings, setOpenSettings] = React.useState(true)
+  const [openSettings, setOpenSettings] = React.useState(false)
   const toggleSettings = () => setOpenSettings(!openSettings);
   return (
     <main className="flex flex-col items-center bg-gray-100">
+    <EditorContainer logs={true} toggleSettings={toggleSettings} >
+    hey
+    </EditorContainer>
     {
       openSettings && (
       <Settings toggleSettings={toggleSettings}/>
       )
       }
-    <EditorContainer logs={true} toggleSettings={toggleSettings} >
-    hey
-    </EditorContainer>
     </main>
   )
 
