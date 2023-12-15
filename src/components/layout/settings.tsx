5c948fa49978eb7eee0483c911c7ca3e996f5cdf
Abdel-manan Abdel-rahman <abdelmanan.abdelrahman03@gmail.com>
2023-12-15 10:54:59 +0300

    type errors

--- src/components/layout/settings.tsx
+++ src/components/layout/settings.tsx
@@ -4,14 +4,14 @@
  * @see https://v0.dev/t/h4DsoLoxN33
  */
 import React from "react"
-import { CardTitle, CardHeader, CardContent, CardFooter, Card } from "@/components/plate-ui/card"
+import { CardTitle, CardHeader, CardContent, CardFooter, Card } from "@/components/ui/card"
 import { ChatBubbleIcon, CursorArrowIcon, MoonIcon, SunIcon} from "@radix-ui/react-icons"
 import { Button } from "@/components/plate-ui/button"
-import { Label } from "@/components/plate-ui/label"
-import { Slider } from "@/components/plate-ui/slider"
+import { Label } from "@/components/ui/label"
+import { Slider } from "@/components/ui/slider"
 import { Toggle } from "@/components/plate-ui/toggle"
 import {Combobox} from "@/components/atoms/combobox"
-import { useStore } from '@/lib/store';
+import { EditorState, useStore } from '@/lib/store';
  import { shallow } from 'zustand/shallow';
 
 const fonts = [
@@ -29,15 +29,15 @@ const fonts = [
   },
   // more will be added later
 ]
-export default function Component({setOpenSettings}: {setOpenSettings:React.Dispatch<React.SetStateAction<boolean>>}) {
+export default function Component({toggleSettings}: {toggleSettings:(opened: boolean)=>void}) {
   const [fontFamily, fontSize, theme,
-  setfontFamily, setfontSize, settheme] = useStore(state => [
+  setfontFamily, setfontSize, settheme] = useStore((state:EditorState) => [
   state.fontFamily, state.fontSize, state.theme,
   state.setfontFamily, state.setfontSize, state.settheme], shallow)
 
   const saveSettings = () => {
     alert("Settings saved!")
-    setOpenSettings(false)
+    toggleSettings(false)
   }
   return (
     <Card className="absolute flex flex-col h-[80%] mx-auto bg-white shadow-lg rounded-lg overflow-hidden px-5 py-5">
@@ -92,7 +92,7 @@ export default function Component({setOpenSettings}: {setOpenSettings:React.Disp
       </CardContent>
       <CardFooter className="border-t p-6 flex justify-between">
         <Button variant="outline" onClick={saveSettings}>Save Settings</Button>
-        <Button onClock={()=>setOpenSettings(false)}>
+        <Button onClick={()=>toggleSettings(false)}>
           Close settings
         </Button>
       </CardFooter>
@@ -100,7 +100,7 @@ export default function Component({setOpenSettings}: {setOpenSettings:React.Disp
   )
 }
 
-function AlignCenterIcon(props) {
+function AlignCenterIcon(props:any) {
   return (
     <svg
       {...props}
@@ -122,7 +122,7 @@ function AlignCenterIcon(props) {
 }
 
 
-function AlignJustifyIcon(props) {
+function AlignJustifyIcon(props:any) {
   return (
     <svg
       {...props}
@@ -144,7 +144,7 @@ function AlignJustifyIcon(props) {
 }
 
 
-function AlignLeftIcon(props) {
+function AlignLeftIcon(props:any) {
   return (
     <svg
       {...props}
@@ -166,7 +166,7 @@ function AlignLeftIcon(props) {
 }
 
 
-function AlignRightIcon(props) {
+function AlignRightIcon(props:any) {
   return (
     <svg
       {...props}
@@ -188,29 +188,8 @@ function AlignRightIcon(props) {
 }
 
 
-function BombIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="11" cy="13" r="9" />
-      <path d="m19.5 9.5 1.8-1.8a2.4 2.4 0 0 0 0-3.4l-1.6-1.6a2.41 2.41 0 0 0-3.4 0l-1.8 1.8" />
-      <path d="m22 2-1.5 1.5" />
-    </svg>
-  )
-}
 
-
-function MenuIcon(props) {
+function MenuIcon(props:any) {
   return (
     <svg
       {...props}
@@ -230,48 +209,3 @@ function MenuIcon(props) {
     </svg>
   )
 }
-
-
-function PanelTopCloseIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
-      <line x1="3" x2="21" y1="9" y2="9" />
-      <path d="m9 16 3-3 3 3" />
-    </svg>
-  )
-}
-
-
-function TypeIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <polyline points="4 7 4 4 20 4 20 7" />
-      <line x1="9" x2="15" y1="20" y2="20" />
-      <line x1="12" x2="12" y1="4" y2="20" />
-    </svg>
-  )
-}
-
